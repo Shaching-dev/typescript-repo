772be3361bef80234b6238fefd99d35cda1469d7
unknown <shaching.contact@gmail.com>
2026-05-12 14:21:50 +0600

    rest tuples

---
+++ dist/15-rest-tuples.js
@@ -0,0 +1,16 @@
+"use strict";
+const num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(...num1, 1200, 400);
+// rest parameter
+function sumAllNumbers(...num) {
+    return num.reduce((s, n) => s + n, 0);
+}
+// console.log(sumAllNumbers(2, 3, 4, 5, 6, 7, 8));
+// rest tuples----->
+function makeRange(...args) {
+    const [start, end, step] = args;
+    const out = [];
+    for (let n = start; n <= end; n += step ?? 0)
+        out.push(n);
+    return out;
+}
---
+++ src/15-rest-tuples.ts
@@ -0,0 +1,23 @@
+const num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// console.log(...num1, 1200, 400);
+
+// rest parameter
+
+function sumAllNumbers(...num: number[]): number {
+  return num.reduce((s, n) => s + n, 0);
+}
+
+// console.log(sumAllNumbers(2, 3, 4, 5, 6, 7, 8));
+
+// rest tuples----->
+
+function makeRange(
+  ...args: [start: number, end: number, step?: number]
+): number[] {
+  const [start, end, step] = args;
+  const out: number[] = [];
+  for (let n = start; n <= end; n += step ?? 0) out.push(n);
+
+  return out;
+}
